1fd0c549ec7ba6d868e63cd437cf8c797c8ee1f6
mmdjian <taiduomao@gmail.com>
2026-04-02 19:58:48 +0800

    fix: MainActivity extends FragmentActivity to prevent ClassCastException on login screen

--- app/src/main/java/com/resident/app/MainActivity.kt
+++ app/src/main/java/com/resident/app/MainActivity.kt
@@ -1,13 +1,13 @@
 package com.resident.app
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.fragment.app.FragmentActivity
 import androidx.navigation.compose.rememberNavController
 import com.resident.app.data.export.ExcelExporter
 import com.resident.app.ui.navigation.NavGraph
@@ -19,7 +19,7 @@ import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
 @AndroidEntryPoint
-class MainActivity : ComponentActivity() {
+class MainActivity : FragmentActivity() {
 
     @Inject
     lateinit var excelExporter: ExcelExporter
